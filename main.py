7b824bf12f72eeaef4e4f29216dd10202dc2e429
Sandip Patel <sandipatel@hotmail.com>
2019-08-04 20:59:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ def show_state(secret_word, guessed):
         else:
             print('_', end=' ')
             
-def make_guess(available_guesses, guessed):
+def make_guess(secret_word, available_guesses, guessed):
     global guesses_left
     
     print('')
     print('')
-    print('make a guess from {}'.format(available_guesses))
+    print('make a guess from {}'.format(' '.join(available_guesses)))
     print('{} guesses_left'.format(guesses_left))
 
     user_guess = input().upper()
-
+    
+    if not user_guess in secret_word:
+        guesses_left = guesses_left - 1         
+        
     available_guesses = available_guesses.replace(user_guess, '')
     guessed = guessed + user_guess
-    
-    guesses_left = guesses_left - 1 
     
     return available_guesses, guessed
 
@@ -28,10 +29,10 @@ def make_guess(available_guesses, guessed):
 words = ['LANFRANC', 'WINTERBOURNE', 'HERTFORDSHIRE', 'BRUNEL', 'CITY']
 available_guesses = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 guessed = ''
-guesses_left = 10
+guesses_left = 3
 
 secret_word = random.choice(words)
 
 while guesses_left !=0:
     show_state(secret_word, guessed)
-    available_guesses, guessed = make_guess(available_guesses, guessed)
+    available_guesses, guessed = make_guess(secret_word, available_guesses, guessed)
